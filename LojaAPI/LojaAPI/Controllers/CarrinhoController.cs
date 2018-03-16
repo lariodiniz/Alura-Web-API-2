c131ff7699f45039c217cee3877b5ba1850fe899
LarioDiniz <lario.diniz@palo.com.br>
2018-03-16 14:41:59 -0300

    Finalizado
    Finalizado

--- LojaAPI/LojaAPI/Controllers/CarrinhoController.cs
+++ LojaAPI/LojaAPI/Controllers/CarrinhoController.cs
@@ -11,18 +11,63 @@ namespace LojaAPI.Controllers
 {
     public class CarrinhoController : ApiController
     {
-        public Carrinho Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            CarrinhoDAO dao = new CarrinhoDAO();
-            Carrinho carrinho = dao.Busca(id);
-            return carrinho;
+            try
+            {
+                CarrinhoDAO dao = new CarrinhoDAO();
+                Carrinho carrinho = dao.Busca(id);
+                return Request.CreateResponse(HttpStatusCode.OK, carrinho);
+            }
+            catch (KeyNotFoundException)
+            {
+
+                string messagem = string.Format($"O carrinho {id} não foi encontrado.");
+                HttpError error =  new HttpError(messagem);
+
+                return Request.CreateResponse(HttpStatusCode.NotFound, error);
+
+            }
+
         }
 
-        public string Post([FromBody] Carrinho carrinho)
+        public HttpResponseMessage Post([FromBody] Carrinho carrinho)
         {
             CarrinhoDAO dao = new CarrinhoDAO();
             dao.Adiciona(carrinho);
-            return "sucesso";
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+            string location = Url.Link("DefaultApi", new { controler = carrinho, id = carrinho.Id });
+            response.Headers.Location = new Uri(location);
+
+            return response;
+        }
+
+        [Route("api/carrinho/{idCarrinho}/produto/{idProduto}")]
+        public HttpResponseMessage Delete([FromUri] int idCarrinho, [FromUri] int idProduto )
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            Carrinho carrinho = dao.Busca(idCarrinho);
+            carrinho.Remove(idProduto);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+        /*
+        [Route("api/carrinho/{idCarrinho}/produto/{idProduto}")]
+        public HttpResponseMessage Put([FromBody]Produto produto, [FromUri] int idCarrinho, [FromUri] int idProduto)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            Carrinho carrinho = dao.Busca(idCarrinho);
+            carrinho.Troca(produto);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }*/
+
+        [Route("api/carrinho/{idCarrinho}/produto/{idProduto}/quantidade")]
+        public HttpResponseMessage Put([FromBody]Produto produto, [FromUri] int idCarrinho, [FromUri] int idProduto)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            Carrinho carrinho = dao.Busca(idCarrinho);
+            carrinho.TrocaQuantidade(produto);
+            return Request.CreateResponse(HttpStatusCode.OK);
         }
     }
     
--- LojaApiClient/LojaApiClient/Program.cs
+++ LojaApiClient/LojaApiClient/Program.cs
@@ -14,29 +14,52 @@ namespace LojaApiClient
         static void Main(string[] args)
         {
             string conteudo;
-            string url = "http://localhost:54605/api/carrinho/1";
-
+            string url = "http://localhost:54605/api/carrinho/200";
             
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "POST";
+            request.Method = "GET";
+            request.Accept = "application/xml";
+            request.ContentType = "application/xml";
 
-            string json = "{'Produtos':[{'Id':6237,'Preco':4000.0,'Nome':'Xbox','Quantidade':3}],'Endereço':'Rua Vergueiro 3185, 8 andar, Sao Paulo','Id':2}";
-            byte[] jsonBytes = Encoding.UTF8.GetBytes(json);
-            request.GetRequestStream().Write(jsonBytes, 0, jsonBytes.Length);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-            request.ContentType = "application/json";
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Headers["Location"]);
+            Console.Read();
+        }
+        public void DeletaProduto()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:63275/api/carrinho/1/produto/3467");
+            request.Method = "DELETE";
 
-            WebResponse response = request.GetResponse();
-            using (Stream responseStream = response.GetResponseStream())
-            {
-                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
-                conteudo = reader.ReadToEnd();
-            }
-            Console.Write(conteudo);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(response.StatusCode);
+
             Console.Read();
         }
 
-        public void testaPostJSON(string[] args)
+        public void trocaQuantidadeProduto()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:63275/api/carrinho/1/produto/6237/quantidade");
+            request.Method = "PUT";
+
+            string xml = "[código XML aqui ... <Quantidade>10</Quantidade>...]";
+            byte[] xmlBytes = Encoding.UTF8.GetBytes(xml);
+            request.GetRequestStream().Write(xmlBytes, 0, xmlBytes.Length);
+
+            request.ContentType = "Application/xml";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(response.StatusCode);
+
+            Console.Read();
+        }
+
+        
+
+        public void testaPostJSON()
         {
             string conteudo;
             string url = "http://localhost:54605/api/carrinho/1";
@@ -69,7 +92,7 @@ namespace LojaApiClient
             request.Method = "POST";
 
             string xml = "<Carrinho xmlns:i='http://www.w3.org/2001/XMLSchema-instance' xmlns='http://schemas.datacontract.org/2004/07/Loja.Models'>" +
-                "< Endereco > Rua Vergueiro 3185, 8 andar, Sao Paulo</ Endereco >< Id > 1 </ Id >< Produtos >< Produto > " +
+                "< Endereco > Rua Vergueiro 3185, 8 andar, Sao Paulo</ Endereco >< Id > 3 </ Id >< Produtos >< Produto > " +
                 "< Id > 123 </ Id > < Nome > Produto criado com POST </ Nome > < Preco > 100 </ Preco > < Quantidade > 1 </ Quantidade >" +
                 " </ Produto > </ Produtos ></ Carrinho > ";
             byte[] xmlBytes = Encoding.UTF8.GetBytes(xml);
